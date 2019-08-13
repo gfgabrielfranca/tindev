035d0865dc87967da2de344b7fccbe3feba504c0
Gabriel França <dev.gabrielfranca@gmail.com>
2019-08-12 22:03:17 -0300

    First commit

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes')
+
+require('dotenv/config');
+
+const server = express();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true
+});
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
